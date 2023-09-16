b238cb67aa5b288fa6aa034bf96871f4629614a9
yanluoluo <1799191542@qq.com>
2023-09-17 02:06:43 +0800

    factor:优化调整调用命令的逻辑

--- main.js
+++ main.js
@@ -8,25 +8,24 @@ const replaceColor = require("./bin/replaceColor/index.js")
 program
   .name("commander-sharp")
   .description("利用命令行来处理图片")
-  .version("1.0.0-alpha-1.2");
+  .version("1.0.3");
 
 // 压缩
 program.command("compress")
   .description("压缩图片")
-  .argument("[string]","需要处理图片目录路径或者图片路径","./")
-  .option("-q, --quality [number]","压缩率,值越高压缩越严重",30)
-  .option("-o, --out [string]","完成后放置的目录","./")
+  .argument("[input]","需要处理图片目录路径或者图片路径","./")
+  .argument("[output]","完成后放置的目录,默认覆盖原文件","./")
+  .option("-q, --quality [number]","图片的处理质量,0-100",80)
   .action(compress);
 
 // 将图片中的指定颜色换成另一种颜色
 program.command("replaceColor")
   .description("将指定图片中的指定颜色换成指定的颜色")
-  .argument("<string>","需要处理图片目录路径或者图片路径")
-  .argument("<string>","新颜色")
-  .option("-c, --color [string]","指定更改的颜色",undefined)
-  .option("-r, --range [number]","模糊匹配范围,值越高，模糊查找的范围就越大,0-255",5)
-  // .option("--all [boolearn]","透明通道也替换",false)
-  .option("-o, --out [string]","完成后放置的目录,默认覆盖原文件","./")
+  .argument("[input]", "需要处理图片目录路径或者图片路径","./")
+  .argument("[output]", "完成后放置的目录,默认覆盖原文件","./")
+  .option("--target-color [color]", "需要更改的颜色，不指定就是所有颜色", undefined)
+  .option("--new-color [color]", "新的颜色,默认白色", "#ffffff")
+  .option("--range [number]", "模糊匹配范围,值越高，模糊查找的范围就越大,0-255", 5)
   .action(replaceColor);
 
 // 获取可以处理的图片对象
