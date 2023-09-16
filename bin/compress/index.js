172de4770545259b1a34cc971dc254916b11c59c
yanluoluo <1799191542@qq.com>
2023-09-17 00:48:02 +0800

    feat:优化compress的使用方式 修改了compress命令的类型 修改了compresss参数有哪些 让compress使用起来更简单

--- bin/compress/index.js
+++ bin/compress/index.js
@@ -9,39 +9,40 @@ const createDirection = require("../../utils/createDirection")
 /**
  * 压缩目标目录 或者 单个文件
  * @param {string} str 需要处理的图片目录或者图片
- * @param {string | number} options.quality 图片的处理质量
- * @param {string} options.out 图片处理完成后放在哪里,默认覆盖
+ * @param {string} output 图片处理完成后放在哪里
+ * @param {number} options.quality 图片的处理质量
  */
-function compress(str, options) {
-  const result = listFolder(str)
+function compress(target, output, options) {
+  const result = listFolder(target)
+  // 空数组就直接返回一个错误提示
+  if (result.length === 0) return console.log(`${chalk.redBright("No file or dir found!")}`)
   // 预处理图片
   result.forEach(item => {
     const image = sharp(item)
-    // 判断是否存在完成后放置的文件夹
-    const targetDir = path.join("./", options.out)
     image
       .metadata()
       .then(async (metadata) => {
-        const type = metadata.format
-        return image
-          .toFormat(type, { quality: Math.min(Math.max(100 - options.quality,1),100) })
+        image
+          .toFormat(metadata.format, { quality: Math.min(Math.max(options.quality , 1), 100) })
           .toBuffer()
-          .then(data =>{
-            const target = path.join(targetDir, item)
-            createDirection(path.dirname(target))
+          .then(data => {
+            let targetItem = fs.statSync(target).isDirectory() 
+              ? path.join(output, item) 
+              : output==="./"
+                ? item
+                : output
+            createDirection(path.dirname(targetItem))
             fs.writeFile(
-              target,
+              targetItem,
               data,
               (err) => {
-                if (err) console.log((`${chalk.red("*")}\t${target}`)) //失败的显示
-                else console.log((`${chalk.green("*")}\t${target}`)) //成功的显示
+                if (err) console.log((`${chalk.red("*")}\t${targetItem}`)) //失败的显示
+                else console.log((`${chalk.green("*")}\t${targetItem}`)) //成功的显示
               }
             )
           })
       })
   })
 }
-
-
 
 module.exports = compress
