6258858a8343867a42ff162dd0bbc1493587a96c
yanluoluo <1799191542@qq.com>
2023-09-17 02:05:59 +0800

    feat:优化replaceColor的功能 优化range的配置

--- bin/replaceColor/index.js
+++ bin/replaceColor/index.js
@@ -1,79 +1,74 @@
 
 const fs = require("fs")
-const path = require("path")
 const sharp = require("sharp")
-const chalk = require("chalk")
 const Color = require("color")
 
 const listFolder = require("../../utils/listFolder")
-const createDirection = require("../../utils/createDirection")
+const getOutputPath = require("../../utils/getOutputPath")
+const { writeFileCallback } = require("../../utils/callback")
 
 
-// 更换颜色
-module.exports = function (target, newColor, options) {
+/**
+ * 
+ * @param {string} input 输入目标
+ * @param {string} output 输出目标
+ * @param {Color} options.newColor 新颜色
+ * @param {number} options.range 模糊范围
+ * @returns 
+ */
+module.exports = function (input, output, options) {
   // 初始化与参数检测
-  const targetList = listFolder(target)
-  if (targetList.length === 0) return
+  const result = listFolder(input)
+  if (result.length === 0) return console.log(`${chalk.redBright("No file or dir found!")}`)
 
-  const targetDir = path.join("./", options.out)
-  newColor = Color(newColor)
-  // 开始遍历
-  targetList.forEach(async (item) => {
+  // 开始执行
+  result.forEach(async (item) => {
     const image = sharp(item)
-    // 判断是否存在完成后放置的文件夹
-
     const metadata = await image.metadata()
-    const raw = image.raw()
-
-    const resultData = await raw.toBuffer().then(bufferDate => replaceColor(new Uint8ClampedArray(bufferDate.buffer), newColor, options))
-
-    sharp(resultData, {
+    const raw = await image.raw()
+    const bufferData = await raw.toBuffer().then(bufferDate => replaceColor(new Uint8ClampedArray(bufferDate.buffer), Color(options.newColor), metadata.channels, options))
+    const newImageBuffer = await sharp(bufferData, {
       raw: {
         width: metadata.width,
         height: metadata.height,
         channels: metadata.channels
       }
-    })
-      .toFormat(metadata.format)
-      .toBuffer()
-      .then(data => {
-        {
-          const target = path.join(targetDir, item)
-          createDirection(path.dirname(target))
-          fs.writeFile(
-            target,
-            data,
-            (err) => {
-              if (err) console.log((`${chalk.red("*")}\t${target}`)) //失败的显示
-              else console.log((`${chalk.green("*")}\t${target}`)) //成功的显示
-            })
-        }
-      })
+    }).toFormat(metadata.format).toBuffer()
+    
+    outputPath = getOutputPath(input, output, item)
+    fs.writeFile( outputPath, newImageBuffer, writeFileCallback)
   })
 }
 
 
 /**
  * 替换图片中像素
- * @param {Array<number>} data 等待处理的像素
+ * @param {Array<number>} data 等待处理的像素数组
  * @param {Color} newColor 新的颜色
- * @param {} options 配置
+ * @param {number} channels 通道数 一般都是3或者4
+ * @param {number} options.range 模糊范围
+ * @param {number} options.targetColor 目标颜色
  * @returns {Uint8ClampedArray}
  */
-function replaceColor(data, newColor, options) {
-  if (options.color === undefined) {
-    for (let i = 0; i < data.length; i += 4) [data[i], data[i + 1], data[i + 2], data[i + 3]] = [...newColor.color, data[i + 3]]
+function replaceColor(data, newColor, channels, options) {
+  const targetColor = Color(options.targetColor).color
+  const range = options.range
+
+  if (options.targetColor === undefined) {
+    for (let i = 0; i < data.length; i += channels) 
+      for (let j = 0; j < Math.min(channels, 3); j++) 
+        (data[i + j] = newColor.color[j])
   } else {
-    const color = Color(options.color)
-    for (let i = 0; i < data.length; i += 4) {
-      sameColor(color.color, Color([data[i], data[i + 1], data[i + 2]]).color,options.range)
-        ? ([data[i], data[i + 1], data[i + 2], data[i + 3]] = [...newColor.color, data[i + 3]])
-        : ([data[i], data[i + 1], data[i + 2], data[i + 3]] = [data[i], data[i + 1], data[i + 2], data[i + 3]])
+    for (let i = 0; i < data.length; i += channels) {
+      let color = Color([data[i], data[i + 1], data[i + 2]]).color
+      if(sameColor(color, targetColor, range))
+        for (let j = 0; j < Math.min(channels, 3); j++)
+          data[i + j] = newColor.color[j]
     }
   }
-
   return new Uint8ClampedArray(data)
 }
+
 
 /**
  * 判断两个颜色是否相等
@@ -81,10 +76,10 @@ function replaceColor(data, newColor, options) {
  * @param {Color} targeColor 第二个颜色
  * @param {number} range 模糊查找范围0-255
  */
-function sameColor(color, targeColor,range = 5) {
-  range = Math.min(Math.max(range,0),255)
+function sameColor(color, targeColor, range = 5) {
+  range = Math.min(Math.max(range, 0), 255)
   for (let i = 0; i < color.length; i++) {
-    if(color[i]+range < targeColor[i] ||  color[i] - range > targeColor[i]) return false
+    if (color[i] + range < targeColor[i] || color[i] - range > targeColor[i]) return false
   }
   return true
 }
